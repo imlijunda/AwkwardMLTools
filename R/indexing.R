64750bf501b061da97a4f8ddcbaec9d2b6c917c2
Jun <imlijunda@gmail.com>
2018-11-27 06:26:04 +1030

    SwapMissingClass and performance improvement MissingClass.

--- R/indexing.R
+++ R/indexing.R
@@ -9,9 +9,70 @@
 #'
 MissingClass <- function(idx, idx_ref, onehot) {
 
-  cls <- IdxToClass(idx, onehot)
-  cls_ref <- IdxToClass(idx, onehot)
-  diff_cls <- setdiff(cls, cls_ref)
+  if (is.null(idx_ref)) {
+    err_null_arg(idx_ref)
+  }
+  if (is.null(idx)) {
+    return(idx_ref)
+  }
 
-  intersect(diff_cls, cls_ref)
+  classes <- seq_len(ncol(onehot))
+  cls <- classes[as.logical(colSums(onehot[idx, , drop = FALSE]))]
+  cls_ref <- classes[as.logical(colSums(onehot[idx_ref, , drop = FALSE]))]
+
+  setdiff(cls_ref, cls)
+}
+
+#' Swap portion of the missing classes from idx_ref to idx.
+#'
+#' Could be useful to re-construct idx and idx_ref with no missing classes. Set
+#' swap_ratio to 0 results in a relatively (but not globally) minimal swapping.
+#'
+#' @param idx a vector of indices.
+#' @param idx_ref a vector of indices.
+#' @param onehot a onehot table.
+#' @param swap_ratio ratio of swapped elements from idx_ref.
+#' @param swap_random whether swapped elements are randomly selected by sample(),
+#' otherwise elements are selected in index order.
+#'
+#' @return a named list with new idx and idx_ref.
+#' @export
+#'
+SwapMissingClass <- function(idx, idx_ref, onehot,
+                             swap_ratio = 0.1, swap_random = FALSE) {
+
+  if (swap_ratio < 0 || swap_ratio >= 1) {
+    err_invalid_value(swap_ratio)
+  }
+
+  #faster look-up list for indices
+  lookup_idx <- DecodeOnehot(t(onehot), 0L)
+
+  mc <- MissingClass(idx, idx_ref, onehot)
+  while (length(mc)) {
+
+    swap_cls <- mc[1]
+
+    swap_idx <- intersect(idx_ref, lookup_idx[[swap_cls]])
+    nswap <- max(floor(length(swap_idx) * swap_ratio), 1L)
+    if (swap_random) {
+      swap_idx <- sample(swap_idx, size = nswap)
+    } else {
+      swap_idx <- swap_idx[seq_len(nswap)]
+    }
+
+    #put swap_idx to idx
+    idx <- c(idx, swap_idx)
+    #remove swap_idx from idx_ref
+    idx_ref <- setdiff(idx_ref, swap_idx)
+
+    #update missing classes
+    mc <- MissingClass(idx, idx_ref, onehot)
+  }
+
+  ret <- list()
+  ret$idx <- idx
+  ret$idx_ref <- idx_ref
+
+  ret
 }
