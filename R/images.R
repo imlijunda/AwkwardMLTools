6cf48d91427002f088df8cad1e44c4818d6eec1d
Jun <imlijunda@gmail.com>
2018-11-29 00:42:13 +1030

    A generic image loader and slight performance improvement.

---
+++ R/images.R
@@ -0,0 +1,122 @@
+#' Load images and concatenate channels.
+#'
+#' Load images using the given loader() function, and concatenate all images
+#' loaded as a new multi-channel image.
+#'
+#' It is encouraged to provide h and w unless ret.flatten is TRUE, since an extra
+#' loading is run to evaluate them thus reducing performance.
+#'
+#' @param files files to load.
+#' @param loader a function to load the images.
+#' @param h OPTIONAL, height of the image. Default: Evaluated during run.
+#' @param w OPTIONAL, width of the image. Default: Evaluated during run.
+#' @param nch OPTIONAL, total channel number of the concatenated image. Default: Evaluated during run.
+#' @param channel_last whether the images are channel_last format. Default: TRUE
+#' @param ret.flatten whether to return a flatten vector. Default: FALSE
+#'
+#' @return a 3-d array.
+#' @export
+#'
+LoadImage <- function(files, loader, h = NULL, w = NULL, nch = NULL,
+                      channel_last = TRUE, ret.flatten = FALSE) {
+
+  files <- as.character(unlist(files))
+
+  #determine h, w if needed
+  if (!ret.flatten && (is.null(h) || is.null(w))) {
+    tmp <- loader(files[1])
+    d <- dim(tmp)
+    if (channel_last) {
+      h <- d[1]
+      w <- d[2]
+    } else {
+      h <- d[2]
+      w <- d[3]
+    }
+  }
+
+  #load images
+  img <- sapply(files, loader)
+
+  #set dimensions
+  if (ret.flatten) {
+    dim(img) <- NULL
+  } else {
+    if (is.null(nch)) {
+      nch <- length(img) %/% h %/% w
+    }
+    if (channel_last) {
+      dim(img) <- c(h, w, nch)
+    } else {
+      dim(img) <- c(nch, h, w)
+    }
+  }
+
+  img
+}
+
+#' Load a batch of images.
+#'
+#' @details The argument files can be either a list of files to load, or a list
+#' of which each element contains a list of files to load and concatenate by
+#' channel.
+#' The latter can be handy when data is provided in single-channel image files.
+#'
+#' @param files a list of files or a list of files by channel.
+#' @param loader a function to load the images.
+#' @param h height of images.
+#' @param w width of images.
+#' @param nch number of channels of each image.
+#' @param channel_last whether the images are channel_last format. Default: TRUE
+#' @param batch_last whether the batch is indexed in last dimension. Default: FALSE
+#'
+#' @return a 4-d array.
+#' @export
+#'
+LoadImageBatch <- function(files, loader, h, w, nch, channel_last = TRUE,
+                           batch_last = FALSE) {
+
+  n <- length(files)
+  all_img <- sapply(X = files, FUN = LoadImage,
+                    loader = loader, channel_last = channel_last, ret.flatten = TRUE)
+  if (batch_last) {
+    if (channel_last) {
+      dim(all_img) <- c(h, w, nch, n)
+    } else {
+      dim(all_img) <- c(nch, h, w, n)
+    }
+  } else {
+    all_img <- t(all_img)
+    if (channel_last) {
+      dim(all_img) <- c(n, h, w, nch)
+    } else {
+      dim(all_img) <- c(n, nch, h, w)
+    }
+  }
+
+  all_img
+}
+
+#' Estimate memory usage of images.
+#'
+#' @param n number of images to load.
+#' @param h height of image.
+#' @param w width of image.
+#' @param nch number of channels.
+#' @param unit what unit to use, "k" for KB, "m" for MB and "g" for GB,
+#' otherwise bytes.
+#'
+#' @return a numeric.
+#' @export
+#'
+EstimateMem <- function(n, h, w, nch, unit = "k") {
+
+  mem_byte <- 8.0 * h * w * nch * n
+  mem <- switch(substr(toupper(unit), 1L, 1L),
+                K = mem_byte / 1024.0,
+                M = mem_byte / 1048576.0,
+                G = mem_byte / 1073741824.0,
+                mem_byte)
+
+  mem
+}
--- R/images_png.R
+++ R/images_png.R
@@ -1,93 +1,20 @@
-#' Load and concatenate png channels.
-#'
-#' Channel last is assumed. Use aperm to convert to channel first if needed.
-#'
-#' If h or w is not given, an extra loading/evaluation on first channel is done
-#' on first channel unless ret.flatten, so some performance loss I guess.
-#'
-#' @param channels file names for each channel.
-#' @param h OPTIONAL, height of the image.
-#' @param w OPTIONAL, width of the image.
-#' @param ret.flatten return a flatten vector.
-#'
-#' @return a channel last 3-d array.
+#' @rdname LoadImage
 #' @export
 #'
-LoadPNGChan <- function(channels, h = NULL, w = NULL, ret.flatten = FALSE) {
+LoadPNG <- function(files, h = NULL, w = NULL, nch = NULL,
+                    channel_last = TRUE, ret.flatten = FALSE) {
 
-  channels <- as.character(unlist(channels))
-  nch <- length(channels)
-
-  if (!ret.flatten && (is.null(h) || is.null(w))) {
-    tmp <- png::readPNG(channels[1])
-    d <- dim(tmp)
-    h <- d[1]
-    w <- d[2]
-  }
-
-  #not sure if a wrapper is need, lapply is just wierd
-  f <- function(x) png::readPNG(x)
-  img <- sapply(channels, f)
-  if (ret.flatten)
-  {
-    dim(img) <- NULL
-  } else {
-    dim(img) <- c(h, w, nch)
-  }
-
-  img
+  LoadImage(files, loader = png::readPNG, h = h, w = w, nch = nch,
+            channel_last = channel_last, ret.flatten = ret.flatten)
 }
 
-#' Load a list of png.
-#'
-#' This function is essentially an lapply of LoadPNGChan on files.
-#'
-#' @param files a list of filenames or a list of channel filenames.
-#' @param h height of image.
-#' @param w width of image.
-#' @param nch total channels of image.
-#' @param file_idx_first whether to put file index in fisrt dimension, otherwise it's put in last dim.
-#'
-#' @return a 4-d array.
+#' @rdname LoadImageBatch
 #' @export
 #'
-LoadPNGList <- function(files, h, w, nch, file_idx_first = TRUE) {
+LoadPNGBatch <- function(files, h, w, nch, channel_last = TRUE,
+                         batch_last = FALSE) {
 
-  n <- length(files)
-  if (file_idx_first) {
-    all_img <- array(data = 0.0, dim = c(n, h, w, nch))
-    for (i in seq_len(n)) {
-      all_img[i, , , ] <- LoadPNGChan(files[[i]], h, w)
-    }
-  } else {
-    all_img <- sapply(X = files, FUN = LoadPNGChan, ret.flatten = TRUE)
-    dim(all_img) <- c(h, w, nch, n)
-  }
-
-  all_img
+  LoadImageBatch(files, loader = png::readPNG, h = h, w = w, nch = nch,
+                 channel_last = channel_last, batch_last = batch_last)
 }
 
-
-#' Estimate memory usage of images.
-#'
-#' @param n number of images to load.
-#' @param h height of image.
-#' @param w width of image.
-#' @param nch number of channels.
-#' @param unit what unit to use, "k" for KB, "m" for MB and "g" for GB,
-#' otherwise bytes.
-#'
-#' @return a numeric.
-#' @export
-#'
-EstimateMem <- function(n, h, w, nch, unit = "k") {
-
-  mem_byte <- 8.0 * h * w * nch * n
-  mem <- switch(substr(toupper(unit), 1L, 1L),
-                K = mem_byte / 1024.0,
-                M = mem_byte / 1048576.0,
-                G = mem_byte / 1073741824.0,
-                mem_byte)
-
-  mem
-}
