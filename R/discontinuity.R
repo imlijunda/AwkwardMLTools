a1ea1e35692a2d7be7a6c52e5a469a933cb5a64f
Jun <imlijunda@gmail.com>
2018-12-24 12:11:43 +1030

    padding indices.

--- R/discontinuity.R
+++ R/discontinuity.R
@@ -38,3 +38,34 @@ sample_idx <- function(measured, sampled, cmpf = `>=`) {
 
   ans
 }
+
+#' Pad indices to left or right
+#'
+#' @param idx a vector of indices
+#' @param start left most index
+#' @param end right most index
+#'
+#' @return a vector of padded indices
+#' @export
+#'
+#' @examples
+#' n = 1000
+#' xi <- sort(sample(seq(n), 100))
+#' xleft <- pad_idx_left(xi)
+#' xright <- pad_idx_right(xi, n)
+pad_idx_left <- function(idx, start = 1L) {
+
+  n <- length(idx)
+
+  c(start, idx[seq.int(n - 1L)] + 1L)
+}
+
+#' @rdname pad_idx_left
+#' @export
+#'
+pad_idx_right <- function(idx, end = NA) {
+
+  n <- length(idx)
+
+  c(idx[2L:n] - 1L, end)
+}
